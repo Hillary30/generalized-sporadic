621186f38b17afbf33d58b8e26cfa612767ba867
Hillary30 <hillarynguyenco@gmail.com>
2024-05-04 18:46:31 -0500

    Revert "fix: tried to revert cause tests stopped working"
    This reverts commit 17197e358a5e6223883216e61fc4a5c98d47950b.

---
+++ build/test/CMakeFiles/ExampleTests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleTests.dir/ExampleTest.cpp.obj"
+  "CMakeFiles/ExampleTests.dir/ExampleTest.cpp.obj.d"
+  "CMakeFiles/ExampleTests.dir/main_test.cpp.obj"
+  "CMakeFiles/ExampleTests.dir/main_test.cpp.obj.d"
+  "ExampleTests.exe"
+  "ExampleTests.exe.manifest"
+  "ExampleTests.pdb"
+  "libExampleTests.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
